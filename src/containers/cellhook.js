21adf00a7ad6b5a8e7ea76148245d0d43e49cff1
John Yu <john.yu@reisystems.com>
2019-03-08 07:58:46 -0500

    only rerender cell

--- src/containers/cellhook.js
+++ src/containers/cellhook.js
@@ -1,39 +1,25 @@
 import React, { useState } from "react";
-// import { connect } from "react-redux";
-// import { bindActionCreators } from "redux";
 import {} from "../actions/index";
 import classNames from "classnames";
-import { validAlphaNum } from "../helpers/primary";
-
 // import Rx from "rx";
+
 export default function CellHook(props) {
-  //   console.log(props);
-  let { handleKeyDown, theMap } = props;
-  console.log("HEREEEE", theMap);
-  //   const [cellput, setCellput] = useState("");
+  let { handleKeyDown, theMap, row, column } = props;
+  console.log("CELL RERENDER", row, column);
   const [state, setState] = useState(false);
 
-  //   props.handleKeyDown(23, 1, 1);
   function handleKeyDownWrap(e) {
-    console.log(e);
     setState(!state);
     return handleKeyDown(e);
   }
-  console.log(handleKeyDown);
+
   function handleClick(e) {
     console.log("click");
   }
-  //   function handleKeyDown(e) {
-  //     let keyCodeString = String.fromCharCode(e.keyCode);
-  //     let key = e.key;
-  //     if (validAlphaNum(keyCodeString)) setCellput(state => (state += key));
-  //     if (e.keyCode === 8) setCellput(state => (state = ""));
-  //   }
 
   let classes = classNames({
     cell: true
   });
-
   return (
     <td
       tabIndex="1"
@@ -47,7 +33,7 @@ export default function CellHook(props) {
       // onMouseOut={this.handleMouseOut}
       //   onMouseDown={this.handleMouseDown}
     >
-      {theMap}
+      {theMap[row][column]}
     </td>
   );
 }
--- src/containers/map.js
+++ src/containers/map.js
@@ -1,48 +1,27 @@
 import React, { useState } from "react";
 import _ from "lodash";
-// import { connect } from "react-redux";
-// import { bindActionCreators } from "redux";
-// import { initMap } from "../actions/index";
-// import Cell from "./cell";
 import CellHook from "./cellhook";
 import { nestedArray, validAlphaNum } from "../helpers/primary";
 
 export default function Map() {
+  console.log("MAP RERENDER");
   const [theMap, setMap] = useState(() => {
     let initMap = nestedArray(4, 4);
     return initMap;
   });
 
-  // console.log(theMap.map);
-
   let handleKeyDownSetup = _.curry((row, column, e) => {
-    console.log("row", row);
-    console.log("col", column);
-    console.log("E", e)
     let keyCodeString = String.fromCharCode(e.keyCode);
     let key = e.key;
     if (validAlphaNum(keyCodeString)) theMap[row][column] += key
     if (e.keyCode === 8) theMap[row][column] = "";
-    console.log(theMap)
   });
 
-  // function handleKeyDown(e) {
-  //   let row = 1;
-  //   let column = 1;
-  //   console.log("ehere,", e, row, column);
-  //   let keyCodeString = String.fromCharCode(e.keyCode);
-  //   let key = e.key;
-  //   if (validAlphaNum(keyCodeString))
-  //     setMap(theMap => (theMap[(row, column)] += key));
-  //   if (e.keyCode === 8) setMap(theMap => (theMap[(row, column)] = ""));
-  // }
-  console.log("MAP", theMap)
   return (
     <div>
       <table>
         <tbody>
           {theMap.map((item, row) => {
-            console.log("ITEM", item);
             return (
               <tr key={row} className="mapRow">
                 {item.map((subitem, column) => {
@@ -50,9 +29,10 @@ export default function Map() {
                   return (
                     <CellHook
                       key={column}
+                      row={row}
+                      column={column}
                       handleKeyDown={handleKeyDown}
                       theMap={theMap}
-                      // clicked={this.props.clickedMap[row][column]}
                     />
                   );
                 })}
